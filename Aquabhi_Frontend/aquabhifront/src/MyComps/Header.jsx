201fda247bb89b009a10829d1f18915393a76608
Abhishek Kr <atul1732.ak@gmail.com>
2022-09-08 02:25:39 +0530

    Added Routing Mechanism

--- Aquabhi_Frontend/aquabhifront/src/MyComps/Header.jsx
+++ Aquabhi_Frontend/aquabhifront/src/MyComps/Header.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import NavBar from "./NavBar"
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Login from './Login'
+import Register from './Register'
+import {Routes,Route} from 'react-router-dom'
 import "./Header.css"
 
 const Header = () => {
@@ -10,6 +16,13 @@ const Header = () => {
         <p className="subhead">One-stop Solution for All your supply water need...</p>
         </div>
      <NavBar/>
+     <Routes>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/about' element = {<About/>}/>
+        <Route path='/contact' element = {<Contact/>}/>
+        <Route path='/login' element = {<Login/>}/>
+        <Route path='/register' element = {<Register/>}/>
+     </Routes>
     </div>
   )
 }
