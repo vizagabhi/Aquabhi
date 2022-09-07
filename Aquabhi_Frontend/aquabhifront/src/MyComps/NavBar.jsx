4571bc61415553e0fba72a0d0d1cfa16bf75bd86
Abhishek Kr <atul1732.ak@gmail.com>
2022-09-07 20:54:04 +0530

    Added NavLink

--- Aquabhi_Frontend/aquabhifront/src/MyComps/NavBar.jsx
+++ Aquabhi_Frontend/aquabhifront/src/MyComps/NavBar.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
 import "./NavBar.css"
 
 const NavBar = () => {
+
   return (
     <div>
       <ul className='nav-list'>
-        <li className='nav-item'>Home</li>
-        <li className='nav-item'>Contact</li>
-        <li className='nav-item'>About Us</li>
-        <li className='nav-item'>Login</li>
-        <li className='nav-item'>Register</li>
+        <li className='nav-item'><NavLink to='/'>Home</NavLink></li>
+        <li className='nav-item'><NavLink to='/contact'>Contact</NavLink></li>
+        <li className='nav-item'><NavLink to='/about'>About Us</NavLink></li>
+        <li className='nav-item'><NavLink to='/login'>Login</NavLink></li>
+        <li className='nav-item'><NavLink to='/register'>Register</NavLink></li>
       </ul>
     </div>
   )
