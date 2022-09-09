c6a5a98265d2aa875e782804ea37d53c3b78e57d
It$_@bhi <52714124+vizagabhi@users.noreply.github.com>
2022-09-09 11:54:21 +0530

    Devp (#6)
    * my personal branch
    
    * Added Header and NavBar Portion
    
    * Added image in home

--- Aquabhi_Frontend/aquabhifront/src/MyComps/Services.jsx
+++ Aquabhi_Frontend/aquabhifront/src/MyComps/Services.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './Services.css'
+import water from './Images/water-tankers.png'
 
 const Services = () => {
   return (
-    <div>
-      Hi this is Services Page!!! 
+    <div className='services-container'>
+      <div className="services-heading"><h1>Our Services</h1></div>
+      <div className="item-1">
+        <div className="item-1-para"><p>Yeh Paani ka Tanker hai, isme 5000ltr paani att ta hai.
+          Ghar ke Tanki ka paani khatam ho gya ho to humey call kro.
+        </p>
+        </div>
+        <div className="item-1-img"><img src={water} alt="Water Tanker"  /></div>
+      </div>
+
+     
     </div>
   )
 }
